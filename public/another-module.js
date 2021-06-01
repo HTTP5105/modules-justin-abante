b4883031aa208a11f2d606219f657cfa0811d7b9
justin-abante <59781087+justin-abante@users.noreply.github.com>
2021-06-01 10:59:23 -0400

    Add files via upload

---
+++ public/another-module.js
@@ -0,0 +1,15 @@
+
+var theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
+
+var buckland = new HobbitSettlement('Buckland', 2340, 'Gorhendad Oldbuck');
+
+var HobbitSettlement = class {
+	constructor(name, founding, founders) {
+		this.name = name;
+		this.founding = founding;
+		this.founders = founders;
+		this.originalSettlement = { name: 'Eriador', details: eriadorDetails };
+	}
+};
+
+export { theShire, buckland, HobbitSettlement };
---
+++ public/my-module.js
@@ -0,0 +1,4 @@
+import { theShire, buckland } from './my-script';
+
+
+export { theShire, statement };
