6cf621d3acd3b7122979212fb27bb4466716c487
justin-abante <59781087+justin-abante@users.noreply.github.com>
2021-06-01 11:34:06 -0400

    made changes to js

--- public/my-module.js
+++ public/my-module.js
@@ -1,4 +1,5 @@
-import { theShire, buckland } from './my-script';
+import { theShire, buckland } from './another-module.js';
 
+var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
 export { theShire, statement };
